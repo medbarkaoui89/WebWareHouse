30540f5077e68e37bb2d470a05ed233a787824eb
Mohamed Barkaoui <mohamedbs1989@hotmail.com>
2024-12-07 20:53:27 +0300

    add new controllers

---
+++ WebWareHouse/Controllers/ShipmentsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebWareHouse.Data;
+using WebWareHouse.Models;
+
+namespace WebWareHouse.Controllers
+{
+    public class ShipmentsController : Controller
+    {
+        private readonly WareHouseContext _context;
+
+        public ShipmentsController(WareHouseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Shipments
+        public async Task<IActionResult> Index()
+        {
+              return _context.Shipments != null ? 
+                          View(await _context.Shipments.ToListAsync()) :
+                          Problem("Entity set 'WareHouseContext.Shipments'  is null.");
+        }
+
+        // GET: Shipments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Shipments == null)
+            {
+                return NotFound();
+            }
+
+            var shipment = await _context.Shipments
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (shipment == null)
+            {
+                return NotFound();
+            }
+
+            return View(shipment);
+        }
+
+        // GET: Shipments/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Shipments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,IdCom,Tariff,Time,Cost")] Shipment shipment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(shipment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(shipment);
+        }
+
+        // GET: Shipments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Shipments == null)
+            {
+                return NotFound();
+            }
+
+            var shipment = await _context.Shipments.FindAsync(id);
+            if (shipment == null)
+            {
+                return NotFound();
+            }
+            return View(shipment);
+        }
+
+        // POST: Shipments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,IdCom,Tariff,Time,Cost")] Shipment shipment)
+        {
+            if (id != shipment.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(shipment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ShipmentExists(shipment.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(shipment);
+        }
+
+        // GET: Shipments/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Shipments == null)
+            {
+                return NotFound();
+            }
+
+            var shipment = await _context.Shipments
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (shipment == null)
+            {
+                return NotFound();
+            }
+
+            return View(shipment);
+        }
+
+        // POST: Shipments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Shipments == null)
+            {
+                return Problem("Entity set 'WareHouseContext.Shipments'  is null.");
+            }
+            var shipment = await _context.Shipments.FindAsync(id);
+            if (shipment != null)
+            {
+                _context.Shipments.Remove(shipment);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ShipmentExists(int id)
+        {
+          return (_context.Shipments?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
